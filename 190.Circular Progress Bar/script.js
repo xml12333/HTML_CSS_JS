50a8b9644ba4f65f1a3f79dd96109576b62b17a8
Nikolay Trafimenkov <xml12333@gmail.com>
2022-06-27 07:36:49 +0300

    add 187-190

---
+++ 190.Circular Progress Bar/script.js
@@ -0,0 +1,10 @@
+const number = document.getElementById("number");
+let counter = 0;
+setInterval(() => {
+  if (counter === 65) {
+    clearInterval();
+  } else {
+    counter += 1;
+    number.innerHTML = `${counter}%`;
+  }
+}, 30);
