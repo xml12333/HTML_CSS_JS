b1b5f34f989b802f630c8b9e9ad5407cec481d91
Nik T <xml12333@gmail.com>
2022-09-23 09:06:48 +0300

    326 continue

--- 326.Portfolio/js/main.js
+++ 326.Portfolio/js/main.js
@@ -1,9 +1,32 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
 
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
 
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
