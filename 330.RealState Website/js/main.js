27aee3a6ae9eb378ea424966b1c15f98e65203bc
Nik T <xml12333@gmail.com>
2022-10-01 09:24:28 +0300

    330 start

---
+++ 330.RealState Website/js/main.js
@@ -0,0 +1,47 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*=============== SWIPER POPULAR ===============*/
+let swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    loop: 'true',
+
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
