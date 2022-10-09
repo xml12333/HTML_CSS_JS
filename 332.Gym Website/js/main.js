7be89a53f7639875ae3caca51e332aa8f1c7b5fd
Nik T <xml12333@gmail.com>
2022-10-09 08:48:49 +0300

    332 continue

--- 332.Gym Website/js/main.js
+++ 332.Gym Website/js/main.js
@@ -20,23 +20,24 @@ if (navClose) {
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('bg-header') 
-                       : header.classList.remove('bg-header')
-}
-window.addEventListener('scroll', scrollHeader)
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SHOW SCROLL UP ===============*/
@@ -44,5 +45,49 @@ window.addEventListener('scroll', scrollHeader)
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 /*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById("calculate-form"),
+  calculateCm = document.getElementById("calculate-cm"),
+  calculateKg = document.getElementById("calculate-kg"),
+  calculateMessage = document.getElementById("calculate-message");
 
+const calculateBmi = (e) => {
+  e.preventDefault();
+  // Check if the fields have a value
+  if (calculateCm.value === "" || calculateKg.value === "") {
+    // Add and remove color
+    calculateMessage.classList.remove("color-green");
+    calculateMessage.classList.add("color-red");
+    // Show message
+    calculateMessage.textContent = "Fill in the Height and Weight";
+    // Remove message three seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 3000);
+  } else {
+    // BMI Formula
+    const cm = calculateCm.value / 100,
+      kg = calculateKg.value,
+      bmi = Math.round(kg / (cm * cm));
+    // Show your health status
+    if (bmi < 18.5) {
+      // Add color and display message
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your ${bmi} is and you are skinny`;
+    } else if (bmi < 25) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your ${bmi} is and you are healthy`;
+    } else {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your ${bmi} is and you are overweight`;
+    }
+    // To clear the input field
+    calculateCm.value = "";
+    calculateKg.value = "";
+    // Remove message four seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 4000);
+  }
+};
+calculateForm.addEventListener("submit", calculateBmi);
 /*=============== EMAIL JS ===============*/
