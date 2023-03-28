d032da4ab76227b7c33dd7167f3a2f2fea4e1811
Nik T <xml12333@gmail.com>
2023-03-28 09:43:46 +0300

    444

---
+++ 444.Gradient Alphabet/js/script.js
@@ -0,0 +1,11 @@
+function update() {
+    document.querySelectorAll('css-doodle').forEach(function (o) {
+      o.update();
+    });
+  }
+  var interval = setInterval(update, 5000);
+  document.addEventListener('click', function () {
+    clearInterval(interval);
+    update();
+    interval = setInterval(update, 5000);
+  });
