7d21cc6cc6eb4345642377205dc21703645decc4
Nik T <xml12333@gmail.com>
2023-04-18 11:13:33 +0300

    455 about

--- 455.Portfolio/js/main.js
+++ 455.Portfolio/js/main.js
@@ -31,7 +31,13 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
 
