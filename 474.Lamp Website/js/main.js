759cb0ee3d29c93ad9d5bbb82a4045d5596be221
Nik T <xml12333@gmail.com>
2023-05-16 11:05:03 +0300

    474 choose

--- 474.Lamp Website/js/main.js
+++ 474.Lamp Website/js/main.js
@@ -1,64 +1,95 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the bg-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('bg-header') 
-                       : header.classList.remove('bg-header')
-}
-window.addEventListener('scroll', scrollHeader)
-
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the bg-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
-const poularSwiper = new Swiper('.popular__content',{
-    slidePerView: 'auto',
-    centeredSlides: true,
-    loop: true,
-    navigation:{
-        nextEl : '.swiper-button-next',
-        prevEl : '.swiper-button-prev',
-    }
-})
+const poularSwiper = new Swiper(".popular__content", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      centeredSlides: false,
+    },
+  },
+});
 
 /*=============== CHOOSE FAQ ===============*/
+const faqItems = document.querySelectorAll(".choose__faq-item");
+// 1. Select each item
+faqItems.forEach((item) => {
+  const faqHeader = item.querySelector(".choose__faq-header");
+  // 2. Select each button click
+  faqHeader.addEventListener("click", () => {
+    // 7. Select the current faq-open class
+    const openItem = document.querySelector(".faq-open");
+    // 5. Call the toggleItem function
+    toggleItem(item);
+    // 8. Remove the faq-open class from other items
+    if (openItem && openItem != item) {
+      toggleItem(openItem);
+    }
+  });
+});
+// 3. Create function to display the content
+const toggleItem = (item) => {
+  // 3.1. Select each faq content
+  const faqContent = item.querySelector(".choose__faq-content");
+  // 6. If the same item contains the faq-open class, remove
+  if (item.classList.contains("faq-open")) {
+    faqContent.removeAttribute("style");
+    item.classList.remove("faq-open");
+  } else {
+    // 4. Add max-height to the content and add the faq-open class
+    faqContent.style.height = faqContent.scrollHeight + "px";
+    item.classList.add("faq-open");
+  }
+};
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
