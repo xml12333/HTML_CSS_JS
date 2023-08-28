480aa2bbb6c0c2fa748c0dddc9698e1ab190ae41
Nik T <xml12333@gmail.com>
2023-08-28 11:51:43 +0300

    542 end

--- 542.Travel Website/js/script.js
+++ 542.Travel Website/js/script.js
@@ -28,3 +28,23 @@ const header = document.querySelector("[data-header]");
 window.addEventListener("scroll", function () {
   header.classList[this.window.scrollY > 100 ? "add" : "remove"]("active");
 });
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.navbar-item a[href*=' + sectionId + ']')
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active')
+		}else{
+			sectionsClass.classList.remove('active')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
--- README.md
+++ README.md
@@ -553,3 +553,4 @@
 |539| [Login Form](https://github.com/xml12333/HTML_CSS_JS/tree/main/539.Login%20Form)| [Demo](https://nikt.com.ua/projects/htmlCssJs/539.Login%20Form/)| ![Thumbnail](539.Login%20Form/img/info_thumbnail.jpg)|
 |540| [Login Form](https://github.com/xml12333/HTML_CSS_JS/tree/main/540.Login%20Form)| [Demo](https://nikt.com.ua/projects/htmlCssJs/540.Login%20Form/)| ![Thumbnail](540.Login%20Form/img/info_thumbnail.jpg)|
 |541| [Marketing Website](https://github.com/xml12333/HTML_CSS_JS/tree/main/541.Marketing%20Website)| [Demo](https://nikt.com.ua/projects/htmlCssJs/541.Marketing%20Website/)| ![Thumbnail](541.Marketing%20Website/img/info_thumbnail.jpg)|
+|542| [Travel Website](https://github.com/xml12333/HTML_CSS_JS/tree/main/542.Travel%20Website)| [Demo](https://nikt.com.ua/projects/htmlCssJs/542.Travel%20Website/)| ![Thumbnail](542.Travel%20Website/img/info_thumbnail.jpg)|
