9160c98a483bfeac3a8225cc0b7c1007483e7cab
Nik T <xml12333@gmail.com>
2023-08-29 11:11:57 +0300

    543 start

---
+++ 543.Movie Website/js/global.js
@@ -0,0 +1,14 @@
+"use strict";
+// add event on multiple elements
+const addEventOnElement = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+// toggle search box in mobile device || small screen
+const searchBox = document.querySelector("[search-box]");
+const searchTogglers = document.querySelectorAll("[search-toggler]");
+
+addEventOnElement(searchTogglers, "click", function () {
+  searchBox.classList.toggle("active");
+});
