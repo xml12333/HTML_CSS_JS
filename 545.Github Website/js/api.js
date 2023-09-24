bb144ea1e616c1db218b4f2d36f4af936ad6e539
Nik T <xml12333@gmail.com>
2023-09-24 13:00:24 +0300

    545 continue

--- 545.Github Website/js/api.js
+++ 545.Github Website/js/api.js
@@ -4,5 +4,20 @@
  * @copyright Nikolay T. 2023
  */
 
-
-'use strict';
+"use strict";
+/**
+ * Fetch data from server
+ * @param {*} url  API Url [required]
+ * @param {*} sucessCallback Success callback [required]
+ * @param {*} errorCallback Error callback [optional]
+ */
+export async function fetchData(url, sucessCallback, errorCallback) {
+  const response = await fetch(url);
+  if (respoonse.ok) {
+    const data = await response.json();
+    sucessCallback(data);
+  } else {
+    const error = await response.json();
+    errorCallback && errorCallback(error);
+  }
+}
--- 545.Github Website/js/app.js
+++ 545.Github Website/js/app.js
@@ -5,6 +5,9 @@
  */
 
 "use strict";
+
+import { fetchData } from "./api.js";
+
 /**
  * @param {NodeList} elements  Elements node array
  * @param {string} eventType  Event Type e.g.: "click", "mouseover"
@@ -75,9 +78,81 @@ addEventOnElements($tabBtns, "keydown", function (e) {
     this.setAttribute("tabindex", "-1");
     $nextElement.setAttribute("tabindex", "0");
     $nextElement.focus();
-  } else if (e.key==="ArrowLeft" && $previousElement){
+  } else if (e.key === "ArrowLeft" && $previousElement) {
     this.setAttribute("tabindex", "-1");
     $previousElement.setAttribute("tabindex", "0");
     $previousElement.focus();
   }
 });
+
+/**
+ * Work with API
+ */
+
+/**
+ * Search
+ */
+const /** {NodeElement} */ $searchSubmit = document.querySelector(
+    "[data-search-submit]"
+  );
+let /** {String} */ apiUrl = "https://api.github.com/users/xml12333";
+let /** {String} */ repoUrl,
+  followerUrl,
+  followingUrl = "";
+
+const searchUser = function () {
+  if (!$searchField.value) {
+    return;
+  }
+  apiUrl = `https://api.github.com/users/${$searchField.value}`;
+  updateProfile(apiUrl);
+};
+
+$searchSubmit.addEventListener("click", searchUser);
+
+//Search when press Enter key
+$searchField.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") searchUser();
+});
+
+/**
+ * Profile
+ */
+
+const /** {NodeElement} */ $profileCard = document.querySelector(
+    "[data-profile-card]"
+  );
+const /** {NodeElement} */ $repoPanel =
+    document.querySelector("[data-repo-panel]");
+const /** {NodeElement} */ $error = document.querySelector("[data-error]");
+
+window.updateProfile = function (profileUrl) {
+  $error.style.display = "none";
+  document.body.style.overflowY = "visible";
+  $profileCard.innerHTML = `
+    <div class="profile-skeleton">
+      <div class="skeleton avatar-skeleton"></div>
+      <div class="skeleton title-skeleton"></div>
+      <div class="skeleton text-skeleton text-1"></div>
+      <div class="skeleton text-skeleton text-2"></div>
+      <div class="skeleton text-skeleton text-3"></div>
+    </div>
+  `;
+  $tabBtns[0].click();
+  $repoPanel.innerHTML = `
+    <div class="card repo-skeleton">
+      <div class="card-body">
+        <div class="skeleton title-skeleton"></div>
+        <div class="skeleton text-skeleton text-1"></div>
+        <div class="skeleton text-skeleton text-2"></div>
+      </div>
+      <div class="card-footer">
+        <div class="skeleton text-skeleton"></div>
+        <div class="skeleton text-skeleton"></div>
+        <div class="skeleton text-skeleton"></div>
+      </div>
+    </div>
+  `.repeat(6);
+};
+
+// updateProfile(apiUrl);
