24744852fe23dd8044ee26d13b6dc5f836db5316
Nik T <xml12333@gmail.com>
2023-10-02 10:18:29 +0300

    546 continue

--- 546.Recipe Website/js/home.js
+++ 546.Recipe Website/js/home.js
@@ -5,3 +5,24 @@
  */
 
 "use strict";
+/**
+ * Home page Search
+ */
+
+const /** {NodeElement} */ $searchField = document.querySelector(
+    "[data-search-field]"
+  );
+const /** {NodeElement} */ $searchBtn =
+    document.querySelector("[data-search-btn]");
+
+$searchBtn.addEventListener("click", function () {
+  if ($searchField.value)
+    window.location = `./recipes.html?q=${$searchField.value}`;
+});
+
+/**
+ * Search submit when press "Enter"
+ */
+$searchField.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") $searchBtn.click();
+});
