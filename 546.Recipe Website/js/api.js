cdd7f5c7a31a5ee896b35dcf44aaa41284ae0295
Nik T <xml12333@gmail.com>
2023-10-07 11:50:50 +0300

    546 continue

--- 546.Recipe Website/js/api.js
+++ 546.Recipe Website/js/api.js
@@ -5,7 +5,7 @@
  */
 
 "use strict";
-import { app_key, app_id } from "./local_settings";
+import { app_key, app_id } from "./local_settings.js";
 window.ACCESS_POINT = "https://api.edamam.com/api/recipes/v2";
 const /** {String} */ APP_ID = app_id;
 const /** {String} */ APP_KEY = app_key;
@@ -19,13 +19,12 @@ const /** {String} */ TYPE = "public";
 export const fetchData = async function (queries, successCallback) {
   const /** {String} */ query = queries
       ?.join("&")
-      .replace(/, /g, "=")
+      .replace(/,/g, "=")
       .replace(/ /g, "%20")
       .replace(/\+/g, "%2B");
   const /** {String} */ url = `${ACCESS_POINT}?app_id=${APP_ID}&app_key=${APP_KEY}&type=${TYPE}${
       query ? `&${query}` : ""
     }`;
-
   const /** {Object} */ response = await fetch(url);
   if (response.ok) {
     const data = await response.json();
--- 546.Recipe Website/js/global.js
+++ 546.Recipe Website/js/global.js
@@ -15,3 +15,23 @@ window.addEventOnElements = function (elements, eventType, callback) {
   for (const element of elements) element.addEventListener(eventType, callback);
 };
 
+export const /** {Array} */ cardQueries = [
+    ["field", "uri"],
+    ["field", "label"],
+    ["field", "image"],
+    ["field", "totalTime"],
+  ];
+
+/**
+ * Skeleton card
+ */
+
+export const /** {String} */ $skeletonCard = `
+    <div class="card skeleton-card">
+      <div class="skeleton card-banner"></div>
+      <div class="card-body">
+        <div class="skeleton card-title"></div>
+        <div class="skeleton card-text"></div>
+      </div>
+    </div>
+  `;
