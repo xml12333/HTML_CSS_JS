23b55d85b6d180f36f77706831e47f87546a7be3
Nik T <xml12333@gmail.com>
2023-10-23 11:34:22 +0300

    547 continue

--- 547.Stock Media Website/js/global.js
+++ 547.Stock Media Website/js/global.js
@@ -4,3 +4,21 @@
  */
 
 "use strict";
+// import
+import { ripple } from "./utils/ripple.js";
+/**
+ * Header on-scroll state
+ */
+
+const /** {Nodeelement} */ $header = document.querySelector("[data-header]");
+window.addEventListener("scroll", () => {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Add ripple effect
+ */
+const /** {NodeeList} */ $rippleElems =
+    document.querySelectorAll("[data-ripple]");
+
+$rippleElems.forEach(($rippleElems) => ripple($rippleElems));
