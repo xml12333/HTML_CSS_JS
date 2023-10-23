f1e0fead246b0d1d173fa4795da2ae6ba4d7239d
Nik T <xml12333@gmail.com>
2023-10-23 16:50:34 +0300

    547 continue

--- 547.Stock Media Website/js/global.js
+++ 547.Stock Media Website/js/global.js
@@ -6,6 +6,7 @@
 "use strict";
 // import
 import { ripple } from "./utils/ripple.js";
+import { addEventOnElements } from "./utils/event.js";
 /**
  * Header on-scroll state
  */
@@ -22,6 +23,20 @@ const /** {NodeeList} */ $rippleElems =
     document.querySelectorAll("[data-ripple]");
 
 $rippleElems.forEach(($rippleElems) => ripple($rippleElems));
+
+/**
+ * Navbar toggle for mobile screen
+ */
+const /** {NodeList} */ $navTogglers =
+    document.querySelectorAll("[data-nav-toggler]");
+const /** {NodeElement} */ $navbar =
+    document.querySelector("[data-navigation]");
+const /** {NodeElement} */ $scrim = document.querySelector("[data-scrim]");
+
+addEventOnElements($navTogglers, "click", function () {
+  $navbar.classList.toggle("show");
+  $scrim.classList.toggle("active");
+});
 
 /**
  * Filter functionality
