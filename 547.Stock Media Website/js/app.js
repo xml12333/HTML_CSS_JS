bdfd07e53453019c5ee12f5bb0a942f34d02c36d
Nik T <xml12333@gmail.com>
2023-10-26 11:55:48 +0300

    547 continue

--- 547.Stock Media Website/js/app.js
+++ 547.Stock Media Website/js/app.js
@@ -10,6 +10,7 @@
 import { client } from "./api_configure.js";
 import { photoCard } from "./photo_card.js";
 import { gridInit, updateGrid } from "./utils/masonry_grid.js";
+import { videoCard } from "./video_card.js";
 
 /**
  * Render curated photos in home page
@@ -25,5 +26,21 @@ client.photos.curated({ page: 1, per_page: 20 }, (data) => {
   data.photos.forEach((photo) => {
     const /** {NodeElement} */ $photoCard = photoCard(photo);
     updateGrid($photoCard, photoGrid.columnsHeight, photoGrid.$columns);
+  });
+});
+
+/**
+ * Render popular videos in home page
+ */
+const /** {NodeElement} */ $videoGrid =
+    document.querySelector("[data-video-grid]");
+$videoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
+
+client.videos.popular({ per_page: 20 }, (data) => {
+  $videoGrid.innerHTML = "";
+  const /** {Array} */ videoGrid = gridInit($videoGrid);
+  data.videos.forEach((video) => {
+    const /** {NodeElement} */ $videoCard = videoCard(video);
+    updateGrid($videoCard, videoGrid.columnsHeight, videoGrid.$columns);
   });
 });
