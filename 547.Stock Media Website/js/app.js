f6a50a153e87c1340581f31afc350db0501e2164
Nik T <xml12333@gmail.com>
2023-10-27 10:54:18 +0300

    547 continue

--- 547.Stock Media Website/js/app.js
+++ 547.Stock Media Website/js/app.js
@@ -11,7 +11,7 @@ import { client } from "./api_configure.js";
 import { photoCard } from "./photo_card.js";
 import { gridInit, updateGrid } from "./utils/masonry_grid.js";
 import { videoCard } from "./video_card.js";
-
+import { collectionCard } from "./collection_card.js";
 /**
  * Render curated photos in home page
  */
@@ -42,5 +42,18 @@ client.videos.popular({ per_page: 20 }, (data) => {
   data.videos.forEach((video) => {
     const /** {NodeElement} */ $videoCard = videoCard(video);
     updateGrid($videoCard, videoGrid.columnsHeight, videoGrid.$columns);
+  });
+});
+
+/**
+ * Render collection in home page
+ */
+const /** {NodeElement} */ $collectionGrid = document.querySelector(
+    "[data-collection-grid]"
+  );
+client.collections.featured({ per_page: 18 }, (data) => {
+  data.collections.forEach((collection) => {
+    const /** {NodeElement} */ $collectionCard = collectionCard(collection);
+    $collectionGrid.appendChild($collectionCard);
   });
 });
--- 547.Stock Media Website/js/collection_card.js
+++ 547.Stock Media Website/js/collection_card.js
@@ -4,3 +4,33 @@
  */
 
 "use strict";
+/**
+ * Import
+ */
+import { ripple } from "./utils/ripple.js";
+
+/**
+ * Create collection card
+ * @param {Object} collection Collection object
+ * @returns {Node} Collection card
+ */
+export const collectionCard = (collection) => {
+  const /** {String} */ root = window.location.origin;
+  const /** {String} */ loc = window.location.pathname;
+  const /** {String} */ dirName = loc.substring(1, loc.lastIndexOf("/"));
+  const { id, title, media_count } = collection;
+  const /** {NodeElement} */ $card = document.createElement("div");
+  $card.classList.add("grid-card", "two-line", "list-item");
+  $card.setAttribute("title", title);
+  $card.innerHTML = `
+    <div>
+      <h3 class="body-large">
+       ${title}
+      </h3>
+      <p class="body-medium label">${media_count}</p>
+    </div>
+    <a href="${root}/${dirName}/pages/collections/collection_detail.html?id=${id}&title=${title}" class="state-layer"></a>
+  `;
+  ripple($card);
+  return $card;
+};
