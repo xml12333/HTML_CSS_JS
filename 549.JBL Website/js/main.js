ac3ab860d73274fb0fc65b976f26c7beb16ca93a
Nik T <xml12333@gmail.com>
2023-11-18 10:59:26 +0200

    549 continue

--- 549.JBL Website/js/main.js
+++ 549.JBL Website/js/main.js
@@ -29,15 +29,27 @@ const linkAction = () => {
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== ADD BLUR HEADER ===============*/
-const blurHeader = () =>{
-  const header = document.getElementById('header')
+const blurHeader = () => {
+  const header = document.getElementById("header");
   // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
-  this.scrollY >= 50 ? header.classList.add('blur-header') 
-                     : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 /*=============== SWIPER FAVORITES ===============*/
+let swiperFavorite = new Swiper(".favorite__swiper", {
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+  grabCursor: true,
 
+  breakpoints:{
+    768: {
+      slidesPerView: 3,
+    }
+  }
+});
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
