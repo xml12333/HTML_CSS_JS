da2f09593c41715e04de36f213de96333329beac
Nik T <xml12333@gmail.com>
2023-11-25 13:12:20 +0200

    550 continue

--- 550.Book Website/js/main.js
+++ 550.Book Website/js/main.js
@@ -3,7 +3,7 @@ const searchButton = document.getElementById("search-button"),
   searchClose = document.getElementById("search-close"),
   searchContent = document.getElementById("search-content");
 
-/*===== MENU SHOW =====*/
+/*===== SEARCH SHOW =====*/
 /* Validate if constant exists */
 if (searchButton) {
   searchButton.addEventListener("click", () => {
@@ -11,7 +11,7 @@ if (searchButton) {
   });
 }
 
-/*===== MENU HIDDEN =====*/
+/*===== SEARCH HIDDEN =====*/
 /* Validate if constant exists */
 if (searchClose) {
   searchClose.addEventListener("click", () => {
@@ -20,7 +20,25 @@ if (searchClose) {
 }
 
 /*=============== LOGIN ===============*/
+const loginButton = document.getElementById("login-button"),
+  loginClose = document.getElementById("login-close"),
+  loginContent = document.getElementById("login-content");
 
+/*===== LOGIN SHOW =====*/
+/* Validate if constant exists */
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    loginContent.classList.add("show-login");
+  });
+}
+
+/*===== LOGIN HIDDEN =====*/
+/* Validate if constant exists */
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    loginContent.classList.remove("show-login");
+  });
+}
 /*=============== ADD SHADOW HEADER ===============*/
 
 /*=============== HOME SWIPER ===============*/
