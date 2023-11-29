ace2df319453b2e6e0334cd8e936fc7c92216f2c
Nik T <xml12333@gmail.com>
2023-11-29 13:16:01 +0200

    550 continue

--- 550.Book Website/js/main.js
+++ 550.Book Website/js/main.js
@@ -96,7 +96,19 @@ let swiperNew = new Swiper(".new__swiper", {
   },
 });
 /*=============== TESTIMONIAL SWIPER ===============*/
-
+let swiperTestimonial = new Swiper(".testimonial__swiper", {
+  loop: true,
+  spaceBeetween: 16,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+      centeredSlides: false,
+    },
+  },
+});
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
