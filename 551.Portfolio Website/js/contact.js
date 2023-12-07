ce39e868292a6204ed372ac95bde7a5969ca514e
Nik T <xml12333@gmail.com>
2023-12-07 13:07:09 +0200

    551 continue

--- 551.Portfolio Website/js/contact.js
+++ 551.Portfolio Website/js/contact.js
@@ -1,1 +1,24 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e)=>{
+    e.preventDefault();
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_sgjqhkw','template_j9b17gd', '#contact-form', 'SFutWE3kaxt2qE2eO')
+    .then(()=>{
+        	// Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅'
+           	// Remove message after five seconds
+            setTimeout(()=>{
+                contactMessage.textContent=''
+            }, 5000)
+        	// Clear input fields
+            contactForm.reset();
+    }, ()=>{
+        	// Show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
--- 551.Portfolio Website/js/index.js
+++ 551.Portfolio Website/js/index.js
@@ -29,7 +29,13 @@ let swiperTestimonial = new Swiper(".testimonial__swiper", {
     dynamicBullets: true,
     clickable: true,
   },
-
-
 });
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
