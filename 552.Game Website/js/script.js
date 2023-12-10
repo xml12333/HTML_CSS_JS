db7c32bda1b507147b719d7ab5e2e814d5cf4b56
Nik T <xml12333@gmail.com>
2023-12-10 11:32:18 +0200

    552 continue

---
+++ 552.Game Website/js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/**
+ * Add event on multiple elements
+ * @param {NodeList} $elements NodeList
+ * @param {String} eventType  Event type string
+ * @param {Function} callback  Callback function
+ */
+window.addEventOnElements = function (elements, eventType, callback) {
+  for (const element of elements) element.addEventListener(eventType, callback);
+};
+
+/**
+ * Mobile navbar
+ * navbar will show after clicking menu button
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+};
+navToggler.addEventListener("click", toggleNav);
+
+const navClose = () => {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+};
+addEventOnElements(navLinks, "click", navClose);
+
+/**
+ * Header
+ * header will be active after scrolled down to 100px of screen
+ */
+
+const header = document.querySelector("[data-header]");
+const activeEl = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+window.addEventListener("scroll", activeEl);
