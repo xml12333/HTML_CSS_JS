856f5934d3cd8c6d5ce057154b328d2625ee55aa
Nik T <xml12333@gmail.com>
2023-12-11 19:25:11 +0200

    552 continue

--- 552.Game Website/js/script.js
+++ 552.Game Website/js/script.js
@@ -32,16 +32,19 @@ const navClose = () => {
 addEventOnElements(navLinks, "click", navClose);
 
 /**
- * Header
- * header will be active after scrolled down to 100px of screen
+ * Header and BACK TOP BTN
+ * header and back top btn will be active after scrolled down to 100px of screen
  */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 const activeEl = function () {
   if (window.scrollY > 100) {
     header.classList.add("active");
+    backTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
+    backTopBtn.classList.remove("active");
   }
 };
 window.addEventListener("scroll", activeEl);
@@ -56,3 +59,21 @@ const buttonHoverRipple = function (event) {
 };
 
 addEventOnElements(buttons, "mousemove", buttonHoverRipple);
+
+/**
+ * Scroll reveal
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal");
+const revealElementOnScroll = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    const isElementInsideWindow =
+      revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.1;
+    if (isElementInsideWindow) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+};
+
+window.addEventListener("scroll", revealElementOnScroll);
+window.addEventListener("load", revealElementOnScroll);
