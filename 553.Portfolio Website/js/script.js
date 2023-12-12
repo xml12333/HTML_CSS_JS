48931daab810253a3728da1a03340786c1af1437
Nik T <xml12333@gmail.com>
2023-12-12 23:02:42 +0200

    553 continue

--- 553.Portfolio Website/js/script.js
+++ 553.Portfolio Website/js/script.js
@@ -33,14 +33,14 @@ addEventOnElements(navTogglers, "click", function () {
   document.body.classList.toggle("nav-active");
 });
 
-addEventOnElements(navLinks,"click",function(){
+addEventOnElements(navLinks, "click", function () {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
   document.body.classList.remove("nav-active");
-})
+});
 
 /**
- * Header 
+ * Header
  * header  will be active after scrolled down to 100px of screen
  */
 
@@ -53,3 +53,25 @@ const activeEl = function () {
   }
 };
 window.addEventListener("scroll", activeEl);
+
+/**
+ * Element tilt effect
+ */
+const tiltElements = document.querySelectorAll("[data-tilt]");
+const initTilt = function (event) {
+  /** get tilt element center position */
+  const centerX = this.offsetWidth / 2;
+  const centerY = this.offsetHeight / 2;
+
+  const tiltPosY = ((event.offsetX - centerX) / centerX) * 10;
+  const tiltPosX = ((event.offsetY - centerY) / centerY) * 10;
+
+  this.style.transform = `perspective(1000px) rotateX(${tiltPosX}deg) rotateY(${
+    tiltPosY - tiltPosY * 2
+  }deg)`;
+};
+addEventOnElements(tiltElements, "mousemove", initTilt);
+
+addEventOnElements(tiltElements, "mouseout", function () {
+  this.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
+});
