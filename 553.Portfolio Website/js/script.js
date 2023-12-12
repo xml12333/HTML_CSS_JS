610eef2556db270f3dcc43122c159846d904be2c
Nik T <xml12333@gmail.com>
2023-12-12 16:08:59 +0200

    553 continue

--- 553.Portfolio Website/js/script.js
+++ 553.Portfolio Website/js/script.js
@@ -8,3 +8,48 @@ window.addEventListener("DOMContentLoaded", function () {
   preloader.classList.add("loaded");
   this.document.body.classList.add("loaded");
 });
+
+/**
+ * Add event on multiple elements
+ * @param {NodeList} $elements NodeList
+ * @param {String} eventType  Event type string
+ * @param {Function} callback  Callback function
+ */
+window.addEventOnElements = function (elements, eventType, callback) {
+  for (const element of elements) element.addEventListener(eventType, callback);
+};
+
+/**
+ * Mobile navbar toggle
+ */
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+addEventOnElements(navTogglers, "click", function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+});
+
+addEventOnElements(navLinks,"click",function(){
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("nav-active");
+})
+
+/**
+ * Header 
+ * header  will be active after scrolled down to 100px of screen
+ */
+
+const header = document.querySelector("[data-header]");
+const activeEl = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+window.addEventListener("scroll", activeEl);
