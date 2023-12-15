8f82e96dea9713c803c6998c6bc00c5ad3936e35
Nik T <xml12333@gmail.com>
2023-12-15 11:17:51 +0200

    554 continue

--- 554.RealEstate Website/js/script.js
+++ 554.RealEstate Website/js/script.js
@@ -15,3 +15,15 @@ const /** {NodeElement} */ $header = document.querySelector("[data-header]");
 window.addEventListener("scroll", (e) => {
   $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+/**
+ * Add to favorite button toggle
+ */
+const /** {NodeList} */ $toggleBtns =
+    document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach(($toggleBtn) => {
+  $toggleBtn.addEventListener("click", () => {
+    $toggleBtn.classList.toggle("active");
+  });
+});
