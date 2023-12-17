39b5b2e5d8e846a66be78aa52f8ccea95fe2e084
Nik T <xml12333@gmail.com>
2023-12-17 18:06:24 +0200

    555 continue

--- 555.Notepad Website/js/utils.js
+++ 555.Notepad Website/js/utils.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+/**
+ * Add event on multiple elements
+ * @param {NodeList} $elements NodeList
+ * @param {String} eventType  Event type string
+ * @param {Function} callback  Callback function
+ */
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const element of elements) element.addEventListener(eventType, callback);
+};
+
+export { addEventOnElements };
