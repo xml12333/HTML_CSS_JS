0a391c424465ac32b4054a395eb3d44497f550c7
Nik T <xml12333@gmail.com>
2023-12-30 12:16:03 +0200

    560 start

---
+++ 560.Shoe Landing/js/main.js
@@ -0,0 +1,54 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+
+/*=============== SWIPER SHOE ===============*/
+const swiperShoe = new Swiper(".home__swiper", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        translate: [-100, 0, -500],
+        opacity: 0,
+      },
+      next: {
+        translate: [100, 0, -500],
+        opacity: 0,
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+/*=============== SHADOW HEADER ===============*/
