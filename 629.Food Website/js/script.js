c6404670057742f351acadc9221a351fbd20464f
Nik T <xml12333@gmail.com>
2024-03-07 10:42:07 +0200

    629 continue

--- 629.Food Website/js/script.js
+++ 629.Food Website/js/script.js
@@ -25,3 +25,24 @@ const addclass = document.querySelector(".site");
 sTrigger.addEventListener("click", () => {
   addclass.classList.toggle("showsearch");
 });
+
+//Swiper
+const swiperThumb = new Swiper(".thumb-nav", {
+  spaceBetween: 10,
+  slidesPerView: 3,
+  slidesPerGroup: false,
+  breakpoints: {
+    992: {
+      direction: "vertical",
+    },
+  },
+});
+const theSlider = new Swiper(".thumb-big", {
+  slidePerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  thumbs: {
+    swiper: swiperThumb,
+  },
+});
