a5e285e74939e80b82fc0adae2765eea4690d591
Nik T <xml12333@gmail.com>
2024-04-02 11:07:05 +0300

    632 continue

--- 632.Ecommerce Website/js/script.js
+++ 632.Ecommerce Website/js/script.js
@@ -47,3 +47,31 @@ const swiper = new Swiper(".sliderbox", {
     clickable: true,
   },
 });
+// carousel
+const carousel = new Swiper(".carouselbox", {
+  spaceBetween: 30,
+  slidesPerView: "auto",
+  centeredSlides: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    481: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      centeredSlides: false,
+    },
+    640: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      centeredSlides: false,
+    },
+    992: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      centeredSlides: false,
+    },
+  },
+});
