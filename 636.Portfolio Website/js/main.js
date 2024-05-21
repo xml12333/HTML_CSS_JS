2bea4871dbacb87904dfe325dc2d875533c8aca3
Nik T <xml12333@gmail.com>
2024-05-21 10:15:54 +0300

    636 continue

--- 636.Portfolio Website/js/main.js
+++ 636.Portfolio Website/js/main.js
@@ -46,27 +46,39 @@ const contactForm = document.getElementById("contact-form"),
 const sendEmail = (e) => {
   e.preventDefault();
   // serviceID - templateID - #form - publicKey
-    emailjs.sendForm('service_sgjqhkw','template_j9b17gd','#contact-form','SFutWE3kaxt2qE2eO')
-    .then(()=>{
+  emailjs
+    .sendForm(
+      "service_sgjqhkw",
+      "template_j9b17gd",
+      "#contact-form",
+      "SFutWE3kaxt2qE2eO"
+    )
+    .then(
+      () => {
         // Show sent message
-        contactMessage.textContent='Message sent successfully ✅'
+        contactMessage.textContent = "Message sent successfully ✅";
         // Remove message after five seconds
-        setTimeout(()=>{
-            contactMessage.textContent=''},5000
-        )
-   // Clear input fields
-
-   // Show error message
-      Message not sent (service error) ❌
-    })
-   
-   
-
-   
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
 };
 contactForm.addEventListener("submit", sendEmail);
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll("section[id]");
 
