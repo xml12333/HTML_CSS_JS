6238357f8b7e864a3edcfd9ad49a6a762d2912b9
Nik T <xml12333@gmail.com>
2024-07-13 09:02:02 +0300

    682 continue

--- 682. Car Website/js/main.js
+++ 682. Car Website/js/main.js
@@ -51,3 +51,11 @@ const swiperHome = new Swiper(".home__swiper", {
 /*=============== GSAP ANIMATION ===============*/
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the shadow-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
