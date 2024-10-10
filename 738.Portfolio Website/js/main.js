e2cf6c4ae704475fbd0769b424e5b6912dd714ac
Nik T <xml12333@gmail.com>
2024-10-10 10:00:40 +0300

    738 continue

--- 738.Portfolio Website/js/main.js
+++ 738.Portfolio Website/js/main.js
@@ -1,11 +1,39 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
 
+const sendEmail = (e) => {
+  e.preventDefault();
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_sgjqhkw",
+      "template_j9b17gd",
+      "#contact-form",
+      "SFutWE3kaxt2qE2eO"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
 
-/*=============== SHOW SCROLL UP ===============*/ 
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+contactForm.addEventListener("submit", sendEmail);
 
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
