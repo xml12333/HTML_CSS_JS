54a0ecdef9ffc75423cff216e955f505e8f31902
Nik T <xml12333@gmail.com>
2024-11-14 09:02:34 +0200

    763 continue

--- 763.Pizza Website/js/main.js
+++ 763.Pizza Website/js/main.js
@@ -30,14 +30,14 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== ADD SHADOW HEADER ===============*/
-const scrollHeader = () => {
+const shadowHeader = () => {
   const header = document.getElementById("header");
-  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  // When the scroll is greater than 50 viewport height, add the shadow-header class to the header tag
   this.scrollY >= 50
-    ? header.classList.add("scroll-header")
-    : header.classList.remove("scroll-header");
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
 };
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== SWIPER POPULAR ===============*/
 
