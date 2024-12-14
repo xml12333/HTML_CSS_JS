01b5afc8fa0fad2a580561f34d142e132731c9fb
Nik T <xml12333@gmail.com>
2024-12-14 09:18:53 +0200

    766 end

--- 766.Education Website/js/main.js
+++ 766.Education Website/js/main.js
@@ -55,3 +55,35 @@ const closeNav = () => {
   menuBtn.style.display = "inline-block";
 };
 closeBtn.addEventListener("click", closeNav);
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_sgjqhkw",
+      "template_j9b17gd",
+      "#contact-form",
+      "SFutWE3kaxt2qE2eO"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+contactForm.addEventListener("submit", sendEmail);
