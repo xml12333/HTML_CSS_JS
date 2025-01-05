db990def25582a4e6b715b716409cf9f368021bd
Nik T <xml12333@gmail.com>
2025-01-05 09:58:06 +0200

    770 continue

---
+++ 770.Social Media Website/js/script.js
@@ -0,0 +1,57 @@
+// SIDEBAR
+const menuItem = document.querySelectorAll(".menu-item");
+
+// MESSAGES
+const messagesNotification = document.querySelector("#messages-notification");
+const messages = document.querySelector(".messages");
+const message = messages.querySelectorAll(".message");
+const messageSearch = document.querySelector("#message-search");
+
+//==================== SIDEBAR ===============================
+
+// remove active aclass from all menu items
+const changeActiveItem = () => {
+  menuItem.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActiveItem();
+    item.classList.add("active");
+    if (item.id != "notifications") {
+      document.querySelector(".notifications-popup").style.display = "none";
+    } else {
+      document.querySelector(".notifications-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
+    }
+  });
+});
+
+//==================== MESSAGES ===============================
+// searches chats
+const searchMessage = () => {
+  const val = messageSearch.value.toLowerCase();
+  message.forEach((chat) => {
+    let name = chat.querySelector("h5").textContent.toLowerCase();
+    if (name.indexOf(val) != -1) {
+      chat.style.display = "flex";
+    } else {
+      chat.style.display = "none";
+    }
+  });
+};
+// search chat
+messageSearch.addEventListener("keyup", searchMessage);
+
+// hightlight messages card when messages menu item is clicked
+messagesNotification.addEventListener("click", () => {
+  messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+  messagesNotification.querySelector(".notification-count").style.display =
+    "none";
+  setTimeout(() => {
+    messages.style.boxShadow = "none";
+  }, 2000);
+});
