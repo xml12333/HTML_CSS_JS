1de27cda829fa7ed46b1ba786fafd78bcc452299
Nik T <xml12333@gmail.com>
2025-01-06 09:25:02 +0200

    770 continue

--- 770.Social Media Website/js/script.js
+++ 770.Social Media Website/js/script.js
@@ -7,6 +7,12 @@ const messages = document.querySelector(".messages");
 const message = messages.querySelectorAll(".message");
 const messageSearch = document.querySelector("#message-search");
 
+// THEME
+const theme = document.querySelector("#theme");
+const themeModal = document.querySelector(".customize-theme");
+const fontSizes = document.querySelectorAll(".choose-size span");
+var root = document.querySelector(":root");
+
 //==================== SIDEBAR ===============================
 
 // remove active aclass from all menu items
@@ -54,4 +60,59 @@ messagesNotification.addEventListener("click", () => {
   setTimeout(() => {
     messages.style.boxShadow = "none";
   }, 2000);
+});
+
+////=================== THEME/DISPLAY CUSTOMIZATION //===================
+
+// opens modal
+const openThemeModal = () => {
+  themeModal.style.display = "grid";
+};
+// close modal
+const closeThemeModal = (e) => {
+  if (e.target.classList.contains("customize-theme")) {
+    themeModal.style.display = "none";
+  }
+};
+themeModal.addEventListener("click", closeThemeModal);
+theme.addEventListener("click", openThemeModal);
+
+//=================== FONTS =====================
+// remove active class from spans or font size selectors
+const removeSizeSelector = () => {
+  fontSizes.forEach((size) => {
+    size.classList.remove("active");
+  });
+};
+
+fontSizes.forEach((size) => {
+  size.addEventListener("click", () => {
+    removeSizeSelector();
+    let fontSize;
+    size.classList.toggle("active");
+    
+    if (size.classList.contains("font-size-1")) {
+      fontSize = "10px";
+      root.style.setProperty("--sticky-top-left", "5.4rem");
+      root.style.setProperty("--sticky-top-right", "5.4rem");
+    } else if (size.classList.contains("font-size-2")) {
+      fontSize = "13px";
+      root.style.setProperty("--sticky-top-left", "5.4rem");
+      root.style.setProperty("--sticky-top-right", "-7rem");
+    } else if (size.classList.contains("font-size-3")) {
+      fontSize = "16px";
+      root.style.setProperty("--sticky-top-left", "-2rem");
+      root.style.setProperty("--sticky-top-right", "-17rem");
+    } else if (size.classList.contains("font-size-4")) {
+      fontSize = "19px";
+      root.style.setProperty("--sticky-top-left", "-5rem");
+      root.style.setProperty("--sticky-top-right", "-25rem");
+    } else if (size.classList.contains("font-size-5")) {
+      fontSize = "22px";
+      root.style.setProperty("--sticky-top-left", "-10rem");
+      root.style.setProperty("--sticky-top-right", "-35rem");
+    }
+    // change font size of the root html element
+    document.querySelector("html").style.fontSize = fontSize;
+  });
 });
