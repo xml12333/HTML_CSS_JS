41263deb58588fd2e1f0d2ce21d1910e2a044f27
Nik T <xml12333@gmail.com>
2025-01-07 09:54:09 +0200

    770 end

--- 770.Social Media Website/js/script.js
+++ 770.Social Media Website/js/script.js
@@ -12,6 +12,10 @@ const theme = document.querySelector("#theme");
 const themeModal = document.querySelector(".customize-theme");
 const fontSizes = document.querySelectorAll(".choose-size span");
 var root = document.querySelector(":root");
+const colorPalette = document.querySelectorAll(".choose-color span");
+const Bg1 = document.querySelector(".bg-1");
+const Bg2 = document.querySelector(".bg-2");
+const Bg3 = document.querySelector(".bg-3");
 
 //==================== SIDEBAR ===============================
 
@@ -90,7 +94,7 @@ fontSizes.forEach((size) => {
     removeSizeSelector();
     let fontSize;
     size.classList.toggle("active");
-    
+
     if (size.classList.contains("font-size-1")) {
       fontSize = "10px";
       root.style.setProperty("--sticky-top-left", "5.4rem");
@@ -115,4 +119,70 @@ fontSizes.forEach((size) => {
     // change font size of the root html element
     document.querySelector("html").style.fontSize = fontSize;
   });
+});
+
+// remove active class from colors
+const changeActiveColorClass = () => {
+  colorPalette.forEach((colorPicker) => {
+    colorPicker.classList.remove("active");
+  });
+};
+
+// change primary colors
+colorPalette.forEach((color) => {
+  color.addEventListener("click", () => {
+    let primaryHue;
+    changeActiveColorClass();
+    if (color.classList.contains("color-1")) {
+      primaryHue = 252;
+    } else if (color.classList.contains("color-2")) {
+      primaryHue = 52;
+    } else if (color.classList.contains("color-3")) {
+      primaryHue = 352;
+    } else if (color.classList.contains("color-4")) {
+      primaryHue = 152;
+    } else if (color.classList.contains("color-5")) {
+      primaryHue = 202;
+    }
+    color.classList.add("active");
+    root.style.setProperty("--hue", primaryHue);
+  });
+});
+
+// theme BACKGROUND values
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+// changes background color
+const changeBG = () => {
+  root.style.setProperty("--light-color-lightness", lightColorLightness);
+  root.style.setProperty("--white-color-lightness", whiteColorLightness);
+  root.style.setProperty("--dark-color-lightness", darkColorLightness);
+};
+Bg1.addEventListener("click", () => {
+  Bg1.classList.add("active");
+  Bg2.classList.remove("active");
+  Bg3.classList.remove("active");
+  window.location.reload();
+});
+Bg2.addEventListener("click", () => {
+  darkColorLightness = "95%";
+  lightColorLightness = "20%";
+  whiteColorLightness = "15%";
+
+  Bg2.classList.add("active");
+  Bg1.classList.remove("active");
+  Bg3.classList.remove("active");
+  changeBG();
+});
+Bg3.addEventListener("click", () => {
+  darkColorLightness = "95%";
+  lightColorLightness = "10%";
+  whiteColorLightness = "0%";
+
+  Bg3.classList.add("active");
+  Bg1.classList.remove("active");
+  Bg2.classList.remove("active");
+  changeBG();
 });
--- README.md
+++ README.md
@@ -774,3 +774,4 @@
 |767| [Dashboard](https://github.com/xml12333/HTML_CSS_JS/tree/main/767.Dashboard)| [Demo](https://nikt.com.ua/projects/htmlCssJs/767.Dashboard/)|
 |768| [Christmas Website](https://github.com/xml12333/HTML_CSS_JS/tree/main/768.Christmas%20Website)| [Demo](https://nikt.com.ua/projects/htmlCssJs/768.Christmas%20Website/)|
 |769| [Dropdown Menu](https://github.com/xml12333/HTML_CSS_JS/tree/main/769.Dropdown%20Menu)| [Demo](https://nikt.com.ua/projects/htmlCssJs/769.Dropdown%20Menu/)|
+|770| [Social Media Website](https://github.com/xml12333/HTML_CSS_JS/tree/main/770.Social%20Media%20Website)| [Demo](https://nikt.com.ua/projects/htmlCssJs/770.Social%20Media%20Website/)|
