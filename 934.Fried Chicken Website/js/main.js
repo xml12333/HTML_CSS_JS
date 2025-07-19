762a99fbd6c8de3398e598c600e1b14c078a6dff
Nik T <xml12333@gmail.com>
2025-07-19 10:25:05 +0300

    934 continue

--- 934.Fried Chicken Website/js/main.js
+++ 934.Fried Chicken Website/js/main.js
@@ -30,12 +30,61 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== ADD SHADOW HEADER ===============*/
-
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__list a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2000,
+  delay: 300,
+  // reset: true, // Animations repeat
+});
+
+sr.reveal(".home__container, .testimonial__container, .footer__container");
+sr.reveal(".home__title", { delay: 600 });
+sr.reveal(".home__description", { delay: 900 });
+sr.reveal(".home__data .button", { delay: 1200 });
+sr.reveal(".destination__card, .gallery__card", { interval: 100 });
+sr.reveal(".join__data", { origin: "left" });
+sr.reveal(".join__img", { origin: "right" });
