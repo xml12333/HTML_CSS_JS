338bc7db03a5214820816f10cb2bba4bb35557be
Nik T <xml12333@gmail.com>
2025-08-22 08:28:44 +0300

    949 continue

--- 949.Food Website/js/script.js
+++ 949.Food Website/js/script.js
@@ -5,3 +5,39 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: "#prev",
   },
 });
+
+const cartIcon = document.querySelector(".cart-icon");
+const cartTab = document.querySelector(".cart-tab");
+const closeBtn = document.querySelector(".close-btn");
+const cardList = document.querySelector(".card-list");
+
+cartIcon.addEventListener("click", () =>
+  cartTab.classList.add("cart-tab-active")
+);
+closeBtn.addEventListener("click", () =>
+  cartTab.classList.remove("cart-tab-active")
+);
+
+let productList = [];
+const showCards = () => {
+  productList.forEach((product) => {
+    const orderCard = document.createElement("div");
+    orderCard.classList.add("order-card");
+    orderCard.innerHTML = ` <div class="card-image">
+              <img src="${product.image}" alt="img">
+            </div>
+            <h4>${product.name}</h4>
+            <h4 class="price">${product.price}</h4>
+            <a href="#" class="btn">Add to card</a>`;
+    cardList.appendChild(orderCard);
+  });
+};
+const initApp = () => {
+  fetch("../949.Food%20Website/products.json").then((response) =>
+    response.json().then((data) => {
+      productList = data;
+      showCards();
+    })
+  );
+};
+initApp();
