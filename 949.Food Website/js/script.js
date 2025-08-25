995aafefb79c733f6d2cf96968b89df1ffa6f340
Nik T <xml12333@gmail.com>
2025-08-25 09:17:30 +0300

    949 continue

--- 949.Food Website/js/script.js
+++ 949.Food Website/js/script.js
@@ -11,6 +11,8 @@ const cartTab = document.querySelector(".cart-tab");
 const closeBtn = document.querySelector(".close-btn");
 const cardList = document.querySelector(".card-list");
 const cartList = document.querySelector(".cart-list");
+const cartTotal = document.querySelector(".cart-total");
+const cartValue = document.querySelector(".cart-value");
 
 cartIcon.addEventListener("click", () =>
   cartTab.classList.add("cart-tab-active")
@@ -21,6 +23,24 @@ closeBtn.addEventListener("click", () =>
 
 let productList = [];
 let cartProduct = [];
+
+const updateTotals = () => {
+  let totalPrice = 0;
+  let totalQuantity = 0;
+
+  document.querySelectorAll(".item").forEach((item) => {
+    const quantity = parseInt(
+      item.querySelector(".quantity-value").textContent
+    );
+    const price = parseFloat(
+      item.querySelector(".item-total").textContent.replace("$", "")
+    );
+    totalPrice += price;
+    totalQuantity += quantity;
+  });
+  cartTotal.textContent = `$${totalPrice.toFixed(2)}`;
+  cartValue.textContent = totalQuantity;
+};
 
 const showCards = () => {
   productList.forEach((product) => {
@@ -69,6 +89,8 @@ const addToCart = (product) => {
                 </a>
               </div>`;
   cartList.appendChild(cartItem);
+  updateTotals();
+
   const plusBtn = cartItem.querySelector(".plus");
   const quantityValue = cartItem.querySelector(".quantity-value");
   const itemTotal = cartItem.querySelector(".item-total");
@@ -79,6 +101,7 @@ const addToCart = (product) => {
     quantity++;
     quantityValue.textContent = quantity;
     itemTotal.textContent = `$${(price * quantity).toFixed(2)}`;
+    updateTotals();
   });
   minusBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -86,11 +109,13 @@ const addToCart = (product) => {
       quantity--;
       quantityValue.textContent = quantity;
       itemTotal.textContent = `$${(price * quantity).toFixed(2)}`;
+      updateTotals();
     } else {
       cartItem.classList.add("slide-out");
       setTimeout(() => {
         cartItem.remove();
         cartProduct = cartProduct.filter((item) => item.id !== product.id);
+        updateTotals();
       }, 300);
     }
   });
