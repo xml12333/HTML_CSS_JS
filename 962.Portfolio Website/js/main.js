d2ec7a799c099414b85b0dde62f011bc0f5e9ec7
Nik T <xml12333@gmail.com>
2025-09-10 09:00:48 +0300

    962 continue

--- 962.Portfolio Website/js/main.js
+++ 962.Portfolio Website/js/main.js
@@ -1,31 +1,42 @@
 /*=============== HOME SPLIT TEXT ===============*/
+const { animate, text, stagger } = anime;
+const { chars: chars1 } = text.split(".home__profession-1", {
+  chars: true,
+});
+const { chars: chars2 } = text.split(".home__profession-2", {
+  chars: true,
+});
 
-
+animate(chars1, {
+  y: [{ to: ["100%", "0%"] }, { to: "100%", delay: 4000, ease: "in(3)" }],
+  duration: 900,
+  ease: "out(3)",
+  delay: stagger(80),
+  loop: true,
+});
+animate(chars2, {
+  y: [{ to: ["100%", "0%"] }, { to: "100%", delay: 4000, ease: "in(3)" }],
+  duration: 900,
+  ease: "out(3)",
+  delay: stagger(80),
+  loop: true,
+});
 /*=============== SWIPER PROJECTS ===============*/
-
 
 /*=============== WORK TABS ===============*/
 
-
 /*=============== SERVICES ACCORDION ===============*/
-
 
 /*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
 
-
 /*=============== COPY EMAIL IN CONTACT ===============*/
 
-
-/*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
-
+/*=============== CURRENT YEAR OF THE FOOTER ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== CUSTOM CURSOR ===============*/
 
-
 /* Hide custom cursor on links */
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
