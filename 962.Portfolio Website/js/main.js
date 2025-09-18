80820edb66d2fefb56a428d2502119bc51c86c10
Nik T <xml12333@gmail.com>
2025-09-18 09:21:42 +0300

    962 continue

--- 962.Portfolio Website/js/main.js
+++ 962.Portfolio Website/js/main.js
@@ -81,7 +81,16 @@ servicesButtons.forEach((button) => {
   });
 });
 /*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
+// Duplicate images to make the animation work
+const tracks = document.querySelectorAll(".testimonials__content");
+tracks.forEach((track) => {
+  const cards = [...track.children]; // spread to make a static copy
 
+  //Duplicate cards only once
+  for (const card of cards) {
+    track.appendChild(card.cloneNode(true));
+  }
+});
 /*=============== COPY EMAIL IN CONTACT ===============*/
 
 /*=============== CURRENT YEAR OF THE FOOTER ===============*/
