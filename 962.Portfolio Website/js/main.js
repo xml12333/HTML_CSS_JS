f1c4783863b12a92418c2fe70514f91021a82bcd
Nik T <xml12333@gmail.com>
2025-09-21 10:11:02 +0300

    962 continue

--- 962.Portfolio Website/js/main.js
+++ 962.Portfolio Website/js/main.js
@@ -132,7 +132,31 @@ const scrollActive = () => {
 };
 window.addEventListener("scroll", scrollActive);
 /*=============== CUSTOM CURSOR ===============*/
+const cursor = document.querySelector(".cursor");
+let mouseX = 0,
+  mouseY = 0; // Store mouse position
+const cursourMove = () => {
+  // Position the cursor
+  cursor.style.left = `${mouseX}px`;
+  cursor.style.top = `${mouseY}px`;
+  cursor.style.transform = `translate(-50%, -50%)`;
 
+  // Update the cursor animation
+  requestAnimationFrame(cursourMove);
+};
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+cursourMove();
 /* Hide custom cursor on links */
-
+const a = document.querySelectorAll("a");
+a.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    cursor.classList.add("hide-cursor");
+  });
+  item.addEventListener("mouseleave", () => {
+    cursor.classList.remove("hide-cursor");
+  });
+});
 /*=============== SCROLL REVEAL ANIMATION ===============*/
