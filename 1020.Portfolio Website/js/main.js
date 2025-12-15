8610642264fc6e0944ce9f3f4b67d317e99fe16f
Nik T <xml12333@gmail.com>
2025-12-15 08:09:08 +0200

    1020 continue

--- 1020.Portfolio Website/js/main.js
+++ 1020.Portfolio Website/js/main.js
@@ -40,7 +40,14 @@ const typedHome = new Typed("#home-typed", {
 });
 
 /*=============== ADD SHADOW HEADER ===============*/
-
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the shadow-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
 /*=============== CONTACT EMAIL JS ===============*/
 
 /*=============== SHOW SCROLL UP ===============*/
