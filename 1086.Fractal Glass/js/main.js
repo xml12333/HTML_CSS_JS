7fb43f78cf14a0aa7fe7bf08be96e39234972648
Nik T <xml12333@gmail.com>
2026-04-03 08:55:20 +0300

    1086 end

--- 1086.Fractal Glass/js/main.js
+++ 1086.Fractal Glass/js/main.js
@@ -29,5 +29,23 @@ const linkAction = () => {
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-
 /*=============== GSAP ANIMATION ===============*/
+gsap.defaults({ opacity: 0, ease: "power4.out", duration: 1.4 });
+const t1 = gsap.timeline();
+const splitTitle = SplitText.create(".home__title", { type: "chars" });
+t1.from(".home__box", { duration: 0.2 }, ".3")
+  .from(".home__bg", { scale: 0.5 }, ".6")
+  .from(".nav > *", { y: -30 }, "1.5")
+  .from(".home__icon", { y: 90 }, "1.8")
+  .from(splitTitle.chars, { y: 300, autoAlpha: 0, stagger: 0.05 }, "2.1")
+  .from(".home__description", { y: 90 }, "3.3")
+  .from(".home__buttons", { y: 90 }, "3.6")
+  .to(".home__bg", {
+    scale: 1.5,
+    opacity: 1,
+    duration: 8,
+    ease: "power1.inOut",
+    repeat: -1,
+    yoyo: true,
+    transformOrigin: "center center",
+  });
