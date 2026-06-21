9a906613503c4a5f776265ff6645c78fe109839f
xml12333 <xml12333@gmail.com>
2026-06-21 10:41:26 +0300

    1107 end

--- 1107.Portfolio Website/js/main.js
+++ 1107.Portfolio Website/js/main.js
@@ -174,3 +174,48 @@ a.forEach((item) => {
   });
 });
 /*=============== SCROLLREVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "60px",
+  duration: 1200,
+  delay: 300,
+  easing: "cubic-bezier(0.34,1.56,0.64,1)",
+  // reset: true, //Animations repeat
+});
+
+sr.reveal(".home__subtitle");
+sr.reveal(".home__title", { delay: 600 });
+sr.reveal(".home__description", { delay: 900 });
+sr.reveal(".home__box-1", { delay: 1200, rotate: { z: -20 } });
+sr.reveal(".home__box-2", { delay: 1300, rotate: { z: -30 } });
+sr.reveal(".home__box-3", { delay: 1400, rotate: { z: -40 } });
+sr.reveal(".home__img", { delay: 1700, distance: "-60px" });
+sr.reveal(".home__circle", { delay: 2000, distance: "-100px" });
+
+sr.reveal(".about__title");
+sr.reveal(".about__description", { delay: 600 });
+sr.reveal(".about__button", { delay: 900 });
+
+sr.reveal(".work__swiper");
+sr.reveal(".services__card:nth-child(odd)", {
+  interval: 200,
+  origin: "left",
+  distance: "100px",
+});
+sr.reveal(".services__card:nth-child(even)", {
+  interval: 200,
+  origin: "right",
+  distance: "100px",
+});
+
+sr.reveal(".skills__description");
+sr.reveal(".skills__card", { delay: 600, interval: 200 });
+sr.reveal(".skills__profession", { delay: 900 });
+sr.reveal(".skills__list", { delay: 1200, interval: 200 });
+
+sr.reveal(".testimonials__container");
+
+sr.reveal(".contact__form");
+sr.reveal(".contact__link", { delay: 600, interval: 200 });
+
+sr.reveal(".footer__container");
