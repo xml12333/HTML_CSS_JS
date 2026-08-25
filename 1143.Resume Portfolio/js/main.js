f590d9ef190694ffc0ba2fe6a1d4bca88fd6e808
xml12333 <xml12333@gmail.com>
2026-08-25 07:15:30 +0300

    1143

---
+++ 1143.Resume Portfolio/js/main.js
@@ -0,0 +1,7 @@
+/*=============== TABS BUTTONS WITH SMOOTH ANIMATION ===============*/ 
+
+
+/*=============== INITIAL FADE IN ON LOAD ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
